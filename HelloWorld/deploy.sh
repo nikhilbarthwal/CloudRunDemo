08af9c563cc384701372be513f20a8117f0fb96f
Nikhil Barthwal <nikhilbarthwal@yahoo.com>
2020-10-15 10:35:40 -0700

    update

--- HelloWorld/deploy.sh
+++ HelloWorld/deploy.sh
@@ -1,6 +1,6 @@
 # Run Cloud Build
-gcloud builds submit --tag gcr.io/nikhilbarthwal-knative/hello-world
+gcloud builds submit --tag gcr.io/nikhilbarthwal-cloudrun-demo/hello-world
 
 #Deploy
-gcloud beta run deploy --image gcr.io/nikhilbarthwal-knative/hello-world
+gcloud beta run deploy --image gcr.io/nikhilbarthwal-cloudrun-demo/hello-world
 
---
+++ ToPdf/deploy.sh
@@ -0,0 +1,8 @@
+
+# Run Cloud Build
+gcloud builds submit --tag gcr.io/nikhilbarthwal-cloudrun-demo/to-pdf
+
+#Deploy
+#gcloud beta run deploy --image gcr.io/nikhilbarthwal-cloudrun-demo/hello-world
+
+
